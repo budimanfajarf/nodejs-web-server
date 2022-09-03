7d0ed054f1a132e1880587bd24a71728640a1259
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-04 02:01:51 +0700

    Handle request for different routings (Routing Request)

--- server.js
+++ server.js
@@ -4,33 +4,41 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    const { method } = request;
+    const { method, url } = request;
 
-    if (method === 'GET') {
-        response.end('<h1>Hello!</h1>');
+    if (url === '/') {
+        if (method === 'GET') {
+            return response.end('<h1>Ini adalah homepage</h1>');
+        }
+
+        return response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
     }
 
-    if (method === 'POST') {
-        let body = [];
+    if (url === '/about') {
+        if (method === 'GET') {
+            return response.end('<h1>Halo! Ini adalah halaman about</h1>')
+        }
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+        if (method === 'POST') {
+            let body = [];
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`);
-        });
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+            });
+
+            return;
+        }
+
+        return response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
     }
 
-    if (method === 'PUT') {
-        response.end('<h1>Bonjour!</h1>');
-    }
-
-    if (method === 'DELETE') {
-        response.end('<h1>Salam!</h1>');
-    }
+    response.end('<h1>Halaman tidak ditemukan!</h1>');
 }
 
 const server = http.createServer(requestListener);
