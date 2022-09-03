df34db7172070bd74fa592bd8cf711499a183927
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-04 01:23:11 +0700

    Handle response for different method/verb request

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
+
+    const { method } = request;
+
+    if (method === 'POST') {
+        return response.end('<h1>Hai!</h1>');
+    }
+
+    if (method === 'PUT') {
+        return response.end('<h1>Bonjour!</h1>');
+    }
+
+    if (method === 'DELETE') {
+        return response.end('<h1>Salam!</h1>');
+    }
+
+    response.end('<h1>Hello!</h1>');
 }
 
 const server = http.createServer(requestListener);
